1f23548f6c983a1776dadbfe643b44f03e084a01
Daria Artiomenko <daridaraart@gmail.com>
2024-01-22 14:07:25 +0300

    showInfo module

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,7 @@ import MiniSlider from './modules/slider/slider-mini';
 import VideoPlayer from './modules/playVideo';
 import Difference from './modules/difference';
 import Forms from './modules/forms';
+import ShowInfo from './modules/showInfo';
 
 window.addEventListener('DOMContentLoaded', () => {
     const slider = new MainSlider({btns: '.next', container: '.page'});
@@ -45,4 +46,5 @@ window.addEventListener('DOMContentLoaded', () => {
 
     new Difference('.officerold', '.officernew', '.officer__card-item').init();
     new Forms('.form').init();
+    new ShowInfo('.plus__content').init();
 });
